fc2cfc5664ac5eebbd5b1eeb7c4aa20be3103310
cre4tive97 <harryborrison5148@gmail.com>
2021-09-23 21:53:46 +0900

    Todolist.vue checkbox

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -2,11 +2,28 @@
   <div class="todolist">
     <h1 class="todolist__header">To-do List</h1>
     <div class="todolist__content">
-      <li>
-        <div class="todolist__content__checkbox"></div>
-        <span class="todolist__content__title"></span>
+      <li
+        class="todolist__content__list"
+        v-for="(a, i) in savedTodolist"
+        :key="i"
+      >
+        <div
+          class="todolist__content__checkbox"
+          :class="{
+            todolist__content__checkbox__checked: savedTodolist[i].checked,
+          }"
+          @click="checkTodolist(i)"
+        ></div>
+        <span class="todolist__content__title">{{
+          savedTodolist[i].title
+        }}</span>
       </li>
     </div>
+
+    <button class="todolist__create">
+      <i class="fas fa-plus-circle"></i>
+    </button>
+    <button class="todolist__delete"><i class="far fa-trash-alt"></i></button>
   </div>
 </template>
 
@@ -15,23 +32,57 @@ export default {
   name: "Todolist",
   data() {
     return {
-      savedTodo: [],
+      savedTodolist: [{ title: "asdfasdf", date: "11/22", checked: false }],
     };
+  },
+  methods: {
+    checkTodolist(i) {
+      if (this.savedTodolist[i].checked == false) {
+        this.savedTodolist[i].checked = true;
+      } else {
+        this.savedTodolist[i].checked = false;
+        console.log(this.savedTodolist[i].checked);
+      }
+    },
   },
 };
 </script>
 
 <style>
+li {
+  list-style: none;
+}
 .todolist {
-  margin: 0px 1.5rem;
   margin-top: 3rem;
   color: white;
   box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
+  width: 100%;
+  height: 80%;
 }
 
 .todolist__header {
+  margin: 0px 1.5rem;
   margin-bottom: 1rem;
+}
+.todolist__content__list {
+  margin: 0px 1.5rem;
+  display: flex;
+  align-items: center;
+}
+
+.todolist__content__checkbox {
+  width: 20px;
+  height: 20px;
+  background-color: transparent;
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  border-radius: 50%;
+  margin-right: 1rem;
+  transition: all 0.5s;
+}
+.todolist__content__checkbox:hover {
+  cursor: pointer;
+}
+.todolist__content__checkbox__checked {
+  background: white;
 }
 </style>
